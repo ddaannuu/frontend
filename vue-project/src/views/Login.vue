327037fe1b0d45b35cc00678a8606dd1f39d0de3
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-30 22:29:39 +0700

    d

--- vue-project/src/views/Login.vue
+++ vue-project/src/views/Login.vue
@@ -21,16 +21,6 @@
           <label for="password">Password:</label>
           <input type="password" v-model="password" required />
         </div>
-        
-         <div class="captcha-box">
-      
-          <div
-            class="g-recaptcha"
-            :data-sitekey="recaptchaSiteKey"
-            data-theme="light"
-            data-size="normal"
-          ></div>
-        </div>
 
         <div>
           <button type="submit" :disabled="loading">
@@ -51,20 +41,11 @@ export default {
       password: '',
       errors: [],
       successMessage: '',
-      loading: false,
-      recaptchaSiteKey: '6Lc7l5ArAAAAADXv489EhSJzLrPGYZmOFX1f9Jxm'
+      loading: false
     };
   },
   mounted() {
     document.body.classList.add("login");
-
-    if (!window.grecaptcha) {
-      const script = document.createElement('script');
-      script.src = 'https://www.google.com/recaptcha/api.js';
-      script.async = true;
-      script.defer = true;
-      document.head.appendChild(script);
-    }
   },
   beforeUnmount() {
     document.body.classList.remove("login");
@@ -75,22 +56,13 @@ export default {
       this.successMessage = '';
       this.loading = true;
 
-      const captchaResponse = grecaptcha.getResponse();
-
-      if (!captchaResponse) {
-        this.errors.push('Silakan centang reCAPTCHA terlebih dahulu.');
-        this.loading = false;
-        return;
-      }
-
       try {
         const response = await fetch('https://ci3-technologia.azurewebsites.net/index.php/auth/login_api', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
             username: this.username,
-            password: this.password,
-            'g-recaptcha-response': captchaResponse
+            password: this.password
           })
         });
 
@@ -107,7 +79,6 @@ export default {
         this.errors.push('Gagal terhubung ke server.');
       } finally {
         this.loading = false;
-         grecaptcha.reset();
       }
     }
   }
@@ -186,9 +157,4 @@ export default {
 .container-login button:hover {
   background: #584fe0;
 }
-.captcha-box {
-  margin-bottom: 1rem;
-  text-align: center;
-}
-
 </style>
